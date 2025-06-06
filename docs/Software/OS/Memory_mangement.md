918561f76b4e0747d651638de925755568a337fc
Mahdi <mahdimomeni6@gmail.com>
2025-06-06 13:54:11 +0330

    chore: make docs persian

--- docs/Software/OS/Memory_mangement.md
+++
@@ -1,6 +0,0 @@
-
-paging
-
-page table
-
-tlb
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,65 +4,65 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'یادداشت های من',
-  favicon: 'https://github.com/meiti-x.png',
+  title: "یادداشت های من",
+  favicon: "https://github.com/meiti-x.png",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  url: 'https://meiti-x.github.io',
-  baseUrl: '/my-notes',
+  url: "https://meiti-x.github.io",
+  baseUrl: "/my-notes",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'meiti-x', // Usually your GitHub org/user name.
-  projectName: 'my-notes', // Usually your repo name.
+  organizationName: "meiti-x", // Usually your GitHub org/user name.
+  projectName: "my-notes", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'fa',
-    locales: ['fa'],
+    defaultLocale: "fa",
+    locales: ["fa"],
     localeConfigs: {
       fa: {
-        label: 'فارسی',
-        direction: 'rtl', // Right-to-left for Persian
+        label: "فارسی",
+        direction: "rtl", // Right-to-left for Persian
       },
     },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -72,18 +72,18 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'https://github.com/meiti-x.png',
+      image: "https://github.com/meiti-x.png",
       navbar: {
-        title: 'صفحه اصلی',
+        title: "صفحه اصلی",
         items: [
           {
-            href: 'https://github.com/meiti-x',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/meiti-x",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
-    
+
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
