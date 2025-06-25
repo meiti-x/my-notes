d2cef1a45a8c8d4d2c12d6f629ad23178050d606
mahdi momeni <mahdimomeni6@gmail.com>
2025-06-25 11:12:52 +0330

    feat: update doc and add github profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,22 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import GitHubProfile from "@site/src/components/GitHubProfile";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <img src='https://github.com/meiti-x.png' style={{
-          width: '250px',
-          height: '250px',
-          borderRadius: '50%',
-          marginBottom: '20px',
-          boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          border: '3px solid #484848',
-          backgroundColor:'#282828',
-          transition: 'transform 0.3s ease',
-        }} alt={siteConfig.title} />
-         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             شروع به خواندن
           </Link>
         </div>
@@ -39,14 +26,14 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      title={`${siteConfig.title} - Portfolio`}
+      description="Personal portfolio and documentation site"
+    >
       <main>
-        <HomepageFeatures />
+        <GitHubProfile />
       </main>
     </Layout>
   );
